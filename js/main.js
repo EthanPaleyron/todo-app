25c8fb4013e5b16ba5396b05fac98c4f4c6fc49b
SoldatdOR05 <ethan.paleyron@gmail.com>
2023-07-30 14:06:57 +0200

    dark mode

--- js/main.js
+++ js/main.js
@@ -1,96 +1,48 @@
 // DARK MODE
-const body = document.querySelector("body");
 const header = document.querySelector("header");
-const DarkOrLight = document.getElementById("DarkOrLight");
-const bDark = document.getElementById("bDark");
-const bLight = document.getElementById("bLight");
+const DarkOrLight = document.querySelector(".DarkOrLight");
 let isDarkLight = true;
 DarkOrLight.addEventListener("click", () => {
     if (isDarkLight) {
-        bDark.style.display = "none";
-        bLight.style.display = "block";
+        DarkOrLight.style.backgroundImage = "url(public/icon-sun.svg)";
         header.style.backgroundImage = "url('../public/bg-desktop-dark.jpg')";
-        body.style.backgroundColor = "#171823";
-        // body.style.color = "#FFF";
+        document.body.dataset["theme"] = "dark";
         isDarkLight = false;
     } else {
-        bDark.style.display = "block";
-        bLight.style.display = "none";
+        DarkOrLight.style.backgroundImage = "url(public/icon-moon.svg)";
         header.style.backgroundImage = "url('../public/bg-desktop-light.jpg')";
-        body.style.backgroundColor = "#FAFAFA";
-        // body.style.color = "#000";
+        document.body.dataset["theme"] = "light";
         isDarkLight = true;
     }
 })
 
 // CREATE TODO
+const listContainer = document.querySelector("#listContainer");
+const inputText = document.querySelector("#inputText");
+const submit = document.querySelector("#submit");
+const todoCounteur = document.querySelector("#todoCounteur");
+const zeroPosts = document.querySelector(".zero_posts");
+
+submit.addEventListener("click", () => {
+    if (inputText.value === "") {
+        alert("To create an new todo, you must write a text 📝")
+    } else {
+        let newPoste = document.createElement("li");
+        newPoste.className = "posts";
+        listContainer.appendChild(newPoste);
+        newPoste.innerHTML = inputText.value;
+        todoCounteur.innerText++;
+        if (todoCounteur.innerText !== 0) {
+            zeroPosts.style.display = "none";
+        }
+    }
+    inputText.value = "";
+})
+
+
 
 // TODO LIST
-const checkbox = document.getElementById("checkbox");
-const titleTodo = document.getElementById("titleTodo");
-// const checkboxActive = document.getElementsByClassName("checkboxActive");
-checkbox.addEventListener("click" , () => {
-    if (checkbox.checked === true) {
-        titleTodo.style.color = "#9495A5";
-        titleTodo.style.textDecorationLine = "line-through";
-    } else {
-        titleTodo.style.color = "#000";
-        titleTodo.style.textDecorationLine = "none";
-    }
-})
+
+// CLEAR
 
 // MAIN FOOTER
-const all = document.getElementById("all");
-const active = document.getElementById("active");
-const completed = document.getElementById("completed");
-const allM = document.getElementById("allM");
-const activeM = document.getElementById("activeM");
-const completedM = document.getElementById("completedM");
-all.addEventListener("click", () => {
-    all.style.color = "#3A7CFD";
-    active.style.color = "#9495A5";
-    completed.style.color = "#9495A5";
-    allM.style.color = "#3A7CFD";
-    activeM.style.color = "#9495A5";
-    completedM.style.color = "#9495A5";
-})
-active.addEventListener("click", () => {
-    all.style.color = "#9495A5";
-    active.style.color = "#3A7CFD";
-    completed.style.color = "#9495A5";
-    allM.style.color = "#9495A5";
-    activeM.style.color = "#3A7CFD";
-    completedM.style.color = "#9495A5";
-})
-completed.addEventListener("click", () => {
-    all.style.color = "#9495A5";
-    active.style.color = "#9495A5";
-    completed.style.color = "#3A7CFD";
-    allM.style.color = "#9495A5";
-    activeM.style.color = "#9495A5";
-    completedM.style.color = "#3A7CFD";
-})
-allM.addEventListener("click", () => {
-    all.style.color = "#3A7CFD";
-    active.style.color = "#9495A5";
-    completed.style.color = "#9495A5";
-    allM.style.color = "#3A7CFD";
-    activeM.style.color = "#9495A5";
-    completedM.style.color = "#9495A5";
-})
-activeM.addEventListener("click", () => {
-    all.style.color = "#9495A5";
-    active.style.color = "#3A7CFD";
-    completed.style.color = "#9495A5";
-    allM.style.color = "#9495A5";
-    activeM.style.color = "#3A7CFD";
-    completedM.style.color = "#9495A5";
-})
-completedM.addEventListener("click", () => {
-    all.style.color = "#9495A5";
-    active.style.color = "#9495A5";
-    completed.style.color = "#3A7CFD";
-    allM.style.color = "#9495A5";
-    activeM.style.color = "#9495A5";
-    completedM.style.color = "#3A7CFD";
-})
