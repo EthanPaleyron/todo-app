e94a70dd1be1a0b4d572e98d8a224a2f686cc608
SoldatdOR05 <ethan.paleyron@gmail.com>
2023-08-07 17:39:53 +0200

    todo lists completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,78 @@
+// FUNCTION🟡
+// CREATE TODO
+function createNewPosts() {
+    if (inputText.value === "") {
+        alert("To create an new todo, you must write a text 📝")
+    } else {
+        let list = document.createElement("li");
+        list.innerHTML = inputText.value;
+        list.classList = "lists";
+        listContainer.appendChild(list);
+        const clear = document.createElement("span");
+        clear.classList = "clear";
+        list.appendChild(clear);
+        console.log(list);
+
+        let allLists = document.querySelectorAll("li.lists");
+        const copyOFLists = [...allLists];
+        for (let i = 0; i < copyOFLists.length; i++) {
+            copyOFLists[i].style.display = "flex";
+            document.querySelector("#all").checked = true;
+        }
+        listsEmpty();
+    }
+    inputText.value = "";
+    slist(listContainer);
+    counteur("li.lists");
+}
+
+// COUNTEUR REMOVE
+function counteur(todo) {
+    document.querySelector("#todoCounteur").innerText = document.querySelectorAll(todo).length;
+}
+
+// LISTS EMPTY
+function listsEmpty() {
+    const allLists = document.querySelectorAll("li.lists");
+    if (allLists.length === 0) {
+        document.querySelector("#lists_empty").style.display = "flex";
+    } else {
+        document.querySelector("#lists_empty").style.display = "none";
+    }
+}
+
+// DRAG AND DROP LIST
+function slist() {
+    let items = listContainer.getElementsByTagName("li"), current = null;
+
+    for (let item of items) {
+        item.draggable = true;
+
+        item.ondragstart = e => {
+            current = item;
+        };
+
+        item.ondragover = e => e.preventDefault();
+
+        item.ondrop = e => {
+            e.preventDefault();
+            if (item != current) {
+                let currentpos = 0, droppedpos = 0;
+                for (let i = 0; i < items.length; i++) {
+                    if (current == items[i]) { currentpos = i; }
+                    if (item == items[i]) { droppedpos = i; }
+                }
+                if (currentpos < droppedpos) {
+                    item.parentNode.insertBefore(current, item.nextSibling);
+                } else {
+                    item.parentNode.insertBefore(current, item);
+                }
+            }
+        }
+    }
+}
+
+// -------------------------------------------
 // DARK MODE
 const header = document.querySelector("header");
 const DarkOrLight = document.querySelector(".DarkOrLight");
@@ -19,30 +94,75 @@ DarkOrLight.addEventListener("click", () => {
 // CREATE TODO
 const listContainer = document.querySelector("#listContainer");
 const inputText = document.querySelector("#inputText");
-const submit = document.querySelector("#submit");
-const todoCounteur = document.querySelector("#todoCounteur");
-const zeroPosts = document.querySelector(".zero_posts");
 
-submit.addEventListener("click", () => {
-    if (inputText.value === "") {
-        alert("To create an new todo, you must write a text 📝")
-    } else {
-        let newPoste = document.createElement("li");
-        newPoste.className = "posts";
-        listContainer.appendChild(newPoste);
-        newPoste.innerHTML = inputText.value;
-        todoCounteur.innerText++;
-        if (todoCounteur.innerText !== 0) {
-            zeroPosts.style.display = "none";
-        }
+document.querySelector(".create_title").addEventListener("keypress", (e) => {
+    if (e.key === 'Enter') {
+        createNewPosts();
     }
-    inputText.value = "";
+})
+document.querySelector("#submit").addEventListener("click", () => createNewPosts())
+
+// CHECKBOX AND CLEAR
+listContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        e.target.classList.remove("not_checked");
+    } else if (e.target.tagName === "SPAN") {
+        if (confirm("Are you sure to delete this post")) {
+            e.target.parentElement.remove();
+            counteur("li.lists");
+        }
+        listsEmpty();
+    }
 })
 
+// CLEAR COMPLETED
+document.querySelector(".clear_completed").addEventListener("click", () => {
+    if (confirm("Are you sure to remove all completed lists")) {
+        const checked = document.getElementsByClassName("checked");
+        const copyOfChecked = [...checked];
+        for (let i = 0; i < copyOfChecked.length; i++) {
+            copyOfChecked[i].remove();
+        }
+        counteur("li.lists");
+        listsEmpty();
+    }
+})
 
+// ALL, ACTIVE AND COMPLETED
+const all = document.querySelector("#all").addEventListener("click", () => {
+    const allLists = document.querySelectorAll("li.lists");
+    const copyOFLists = [...allLists];
+    for (let i = 0; i < copyOFLists.length; i++) {
+        copyOFLists[i].style.display = "flex";
+        counteur("li.lists");
+    }
+})
 
-// TODO LIST
+const active = document.querySelector("#active").addEventListener("click", () => {
+    const listsChecked = document.querySelectorAll("li.lists:not(.checked)");
+    const listsActive = document.getElementsByClassName("checked");
+    const copyOfNotChecked = [...listsActive];
+    const copyOfChecked = [...listsChecked];
+    for (let i = 0; i < copyOfNotChecked.length; i++) {
+        copyOfNotChecked[i].style.display = "none";
+    }
+    for (let i = 0; i < copyOfChecked.length; i++) {
+        copyOfChecked[i].style.display = "flex";
+    }
+    counteur("li.lists:not(.checked)");
+})
 
-// CLEAR
-
-// MAIN FOOTER
+const completed = document.querySelector("#completed").addEventListener("click", () => {
+    let listsChecked = document.querySelectorAll("li.lists:not(.checked)");
+    let listsActive = document.getElementsByClassName("checked");
+    const copyOfNotChecked = [...listsActive];
+    const copyOfChecked = [...listsChecked];
+    for (let i = 0; i < copyOfChecked.length; i++) {
+        copyOfChecked[i].style.display = "none";
+    }
+    for (let i = 0; i < copyOfNotChecked.length; i++) {
+        copyOfNotChecked[i].style.display = "flex";
+    }
+    counteur("li.checked");
+})
